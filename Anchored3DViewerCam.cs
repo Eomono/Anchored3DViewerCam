db3f9d3bd1771338f469c120cf63399fb143eeb7
Adrian <higare2@gmail.com>
2021-01-27 00:07:44 -0600

    Update Anchored3DViewerCam.cs

--- Anchored3DViewerCam.cs
+++ Anchored3DViewerCam.cs
@@ -1,8 +1,3 @@
-﻿/*** A simple script for a runtime mouse-controlled 3D-Viewer camera akin to Blender's default and similar 3D software.
-Just attach this script to your camera and it should work.
-Sorry for the seemingly arbitrary numerical values for sensivity and smoothness; these were the values that gave me the best results.
-Cheers!***/
-
 using UnityEngine;
 using UnityEngine.EventSystems;
 
